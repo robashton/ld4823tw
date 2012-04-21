ebe4982059fc93776e4a4d2cb4f8a44918e7110f
Rob Ashton <robashton@codeofrob.com>
2012-04-21 09:36:59 +0200

    Common code added

---
+++ server.js
@@ -0,0 +1,25 @@
+var
+  path = require('path'),
+  http = require('http'),
+  paperboy = require('paperboy')
+  swallow = require('swallow')
+
+  PORT = 8003,
+  WEBROOT = path.join(path.dirname(__filename), 'site');
+
+http.createServer(function(req, res) {
+  var ip = req.connection.remoteAddress;
+  paperboy
+    .deliver(WEBROOT, req, res)
+    .otherwise(function(err) {
+      res.writeHead(404, {'Content-Type': 'text/plain'});
+      res.end("Error 404: File not found");
+    });
+}).listen(PORT);
+
+swallow.build({
+  in: 'assets',
+  out: 'site/assets.json'
+}, function() {
+  console.log('assets built');
+});
---
+++ site/game.js
@@ -0,0 +1,151 @@
+(function() {
+
+  var EventContainer = function(defaultContext) {
+    this.handlers = [];
+    this.defaultContext = defaultContext;
+  }; 
+  
+  EventContainer.prototype = {
+    raise: function(source, data) {
+     var handlerLength = this.handlers.length;
+     for(var i = 0; i < handlerLength; i++) {
+        var handler = this.handlers[i];
+        if(!handler) continue;
+        handler.method.call(handler.context || this.defaultContext, data, source);   
+     }
+    },
+    add: function(method, context) {
+      this.handlers.push({
+        method: method,
+        context: context      
+      });
+    },
+    remove: function(method, context) {
+      this.handlers = _(this.handlers).filter(function(item) {
+        return item.method !== method || item.context !== context;
+      });
+    }
+  };
+
+   var Eventable = function() {
+    this.eventListeners = {};
+    this.allContainer = new EventContainer(this);
+    this.eventDepth = 0;
+  };
+  
+  Eventable.prototype = {
+    autoHook: function(container) {
+      for(var key in container) { 
+        if(key.indexOf('on') === 0) {
+          this.on(key.substr(2), container[key], container);
+        }   
+      }
+    },
+    autoUnhook: function(container) {
+      for(var key in container) { 
+        if(key.indexOf('on') === 0) {
+          this.off(key.substr(2), container[key], container);
+        }   
+      }
+    },
+    once: function(eventName, callback, context) {
+      var self = this;
+      var wrappedCallback = function(data, sender) {
+        callback.call(this, data, sender);
+        self.off(eventName, wrappedCallback, context);
+      };
+      this.on(eventName, wrappedCallback, context);
+    },
+    
+    on: function(eventName, callback, context) {
+      this.eventContainerFor(eventName).add(callback, context);
+    },
+    
+    off: function(eventName, callback, context) {
+      this.eventContainerFor(eventName).remove(callback, context);
+    },
+
+    onAny: function(callback, context) {
+      this.allContainer.add(callback, context);
+    },
+
+    offAny: function(callback, context) {
+      this.allContainer.remove(callback, context);
+    },
+
+    raise: function(eventName, data, sender) {
+      var container = this.eventListeners[eventName];
+
+      if(container)
+        container.raise(sender || this, data);
+      
+      this.allContainer.raise(sender || this, {
+        event: eventName,
+        data: data
+      });
+    },
+
+    eventContainerFor: function(eventName) {
+      var container = this.eventListeners[eventName];
+      if(!container) {
+        container =  new EventContainer(this);
+        this.eventListeners[eventName] = container;
+      }
+      return container;
+    }
+  };
+
+  var Resources = function() {
+    Eventable.call(this);
+    this.packages = [];
+    this.cachedResources = {};
+  };
+  Resources.prototype = {
+    load: function(file, cb) {
+      var self = this;  
+      $.getJSON(file, function(data) {
+        self.packages.push(data);
+        cb();
+      })
+    },
+    getTexture: function(path) {
+      var texture = this.fromCacheOrCreate(path, function(data) {
+        var image = new Image();
+        image.src = "data:image/png;base64," + data;
+        return image;
+      });
+      if(!texture)
+        console.warn('Missing texture', path);
+      return texture;
+    },
+    fromCacheOrCreate: function(path, createCallback) {
+      var item = this.cachedResources[path];
+      if(item) return item;
+      var data = this.findData(path);
+      if(data) {
+        item = createCallback(data);
+        this.cachedResources[path] = item;
+      } 
+      return item;
+    },
+    findData: function(path) {
+      for(var i = 0; i < this.packages.length; i++) {
+        var package = this.packages[i];
+        var data = package[path];
+        if(data) return data;
+      }
+      return null;
+    }
+  };
+  _.extend(Resources.prototype, Eventable.prototype);
+  var GlobalResources = new Resources();
+
+
+
+
+
+
+  $(document).ready(function() {
+    console.log('packages loaded');
+  });
+})();
